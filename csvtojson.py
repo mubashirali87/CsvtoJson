1fa26b81683364d283b5e44917809f10ba135e1c
mubashirali87 <44904844+mubashirali87@users.noreply.github.com>
2023-01-22 14:45:55 +0530

    Create csvtojson.py

---
+++ csvtojson.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import csv,json
+data=pd.read_csv("Instagram.csv")
+print(data)
+
+print("Converted JSON file below :")
+
+print (json.dumps(list(csv.reader(open('Instagram.csv')))))
